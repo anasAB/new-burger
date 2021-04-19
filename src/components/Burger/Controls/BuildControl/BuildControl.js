479526952f86ce4bf1dae697bce4659042bd9ebd
anasAB <anas.222@hotmail.com>
2021-04-19 11:25:16 +0200

    adding Control buttons

--- src/components/Burger/Controls/BuildControl/BuildControl.js
+++ src/components/Burger/Controls/BuildControl/BuildControl.js
@@ -2,19 +2,15 @@ import React from 'react';
 import classes from './BuildControl.css'
 
 const buildControl = (props) => {
-    console.log(props.ingredients);
-    let { label, price } = props.ingredients
+    let { label, type, price, count } = props.ingredient
     return (
         <div className={classes.BuildControl}>
             <div className={classes.Label}>{label}</div>
-            <button className={classes.Less}>Less</button>
-            <button className={classes.More}>More</button>
+            <button className={classes.Less} onClick={() => props.removeIngredient(type, price, count)} disabled={props.disabledLessButton}>Less</button>
+            <button className={classes.More} onClick={() => props.addIngredient(type, price, count)}>More</button>
             <div className={classes.Label}>{price}</div>
         </div>
     )
-
-
-
 }
 
 export default buildControl
